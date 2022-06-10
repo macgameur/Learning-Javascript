814d0b6df260fb3e725ac01132f4e15148a73e80
Macgameur <maceo.jalbert@gmail.com>
2022-06-10 09:02:03 +0200

    :feat:

---
+++ js/MyScript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function GetTime() {
+    document.getElementById('demo').innerHTML = Date();
+    window.alert(Date());
+}
